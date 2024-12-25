667dc1e340afb66bcb278fa85ae9d658f6f5bd6a
mikiyuna <132547281+mikiyuna@users.noreply.github.com>
2024-12-26 01:39:39 +0900

    kadai_014を追加する

---
+++ kadai_014/square.js
@@ -0,0 +1,13 @@
+/*
+const calculation = (num) =>{
+    return num * num;
+}
+
+console.log(calculation(10));
+*/
+
+const calculation = (num) =>{
+    console.log(num * num);
+}
+
+calculation(10);
